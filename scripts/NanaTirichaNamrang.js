87840b5f5410353a097736caae883a8b5f2a166d
shrichaitanyanandsaraswati <shrichaitanyanandsaraswati@gmail.com>
2024-07-26 00:14:04 +0530

    Update NanaTirichaNamrang.js
    Fixed Module Error

--- scripts/NanaTirichaNamrang.js
+++ scripts/NanaTirichaNamrang.js
@@ -3,4 +3,6 @@
     { eng: 'shri datta prabodh stav', hin: 'श्री दत्त प्रभोधस्तव' , dir: 'NanaTirichaNamrang', pg: '6', num: '2' },
 ];
 
-module.exports = NanaTirichaNamrang;
+if (typeof module === 'object' && module.exports) {
+    module.exports = NanaTirichaNamrang;
+}
