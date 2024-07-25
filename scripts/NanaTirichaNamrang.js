4255a6bca49a51d76d6137e75b60f1be1979fd28
shrinanabhajan <shrinanabhajan@gmail.com>
2024-07-25 21:06:15 +0530

    u4

--- scripts/NanaTirichaNamrang.js
+++ scripts/NanaTirichaNamrang.js
@@ -1,6 +1,6 @@
 ﻿var NanaTirichaNamrang = [
     { eng: 'kakad arati aarati arti', hin: 'काकड आरती', dir: 'NanaTirichaNamrang', pg: '6', num: '1' },
-    { eng: 'shri datta prabodh stav', hin: 'श्री दत्त प्रभोधस्तव' , dir: 'NanaTirichaNamrang', pg: '6', num: '2' },
+    { eng: 'datta prabodh stav', hin: 'श्री दत्त प्रभोधस्तव' , dir: 'NanaTirichaNamrang', pg: '6', num: '2' },
 ];
 
 module.exports = NanaTirichaNamrang;
