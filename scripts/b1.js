10ed9874cf81b08798d73dd830879872f61be7cf
shrinanabhajan <shrinanabhajan@gmail.com>
2024-08-03 19:39:39 +0530

    Update b1.js

--- scripts/b1.js
+++ scripts/b1.js
@@ -1,6 +1,11 @@
 var b1 = [
     { eng: 'maze gathode', hin: 'माझे माझे चे गाठोडे', dir: 'b1', id: '1', bk: '', pg: '',  },
     { eng: 'hanumant stuti ghatika chaladi ninta', hin: 'श्री हनुमंत स्तुति - घटिका चलादी निंता', dir: 'b1', id: '2', bk: '', pg: '',  },
+{ eng: 'bolava vitthal vithal', hin: 'बोलावा विठ्ठल', dir: 'b1', id: '3', bk: '', pg: '',  },
+
+
+
+
 ];
 
 if (typeof module === 'object' && module.exports) {
