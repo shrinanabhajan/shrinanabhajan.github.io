078cea7a30bef15a2971bde741a1206ebf856d98
shrinanabhajan <shrinanabhajan@gmail.com>
2024-08-03 12:46:00 +0530

    Update ntn.js

--- scripts/ntn.js
+++ scripts/ntn.js
@@ -1,11 +1,13 @@
 ﻿var ntn = [
-    { eng: 'kakad arati bupali', hin: 'काकड आरती-भूपाळी', dir: 'ntn', id: '6.1', bk: 'नाना तिरीचा नामरंग', pg: '6' },
-    { eng: 'kakad arati shri datta prabodh stav', hin: 'काकड आरती-श्री दत्त प्रभोधस्तव' , dir: 'ntn', id: '6.2' , bk: 'नाना तिरीचा नामरंग', pg: '6' },
-    { eng: 'kakad arati aarati arti jay deva shri sadguru dnyan', hin: 'काकड आरती-जय देवा श्री सद्गुरु जय ज्ञान रुपा',  dir: 'ntn', id: '8' , bk: 'नाना तिरीचा नामरंग', pg: '8' },
-    { eng: 'kakad arati shri dattatreya prarthan chashk', hin: 'काकड आरती-श्री दत्तात्रेय प्रार्थना चतुष्क', dir: 'ntn', id: '9', bk: 'नाना तिरीचा नामरंग', pg: '9' },
-    { eng: 'kakad arati manas atm puja pooja', hin: 'काकड आरती-श्री मानस पूजा (श्री आत्मपूजा)', dir: 'ntn', id: '10', bk: 'नाना तिरीचा नामरंग', pg: '10' },     
-    
-];
+        { eng: 'kakad arati bupali', hin: 'काकड आरती-भूपाळी', dir: 'ntn', id: '6.1', bk: 'नाना तिरीचा नामरंग', pg: '6' },
+        { eng: 'kakad arati shri datta prabodh stav', hin: 'काकड आरती-श्री दत्त प्रभोधस्तव' , dir: 'ntn', id: '6.2' , bk: 'नाना तिरीचा नामरंग', pg: '6' },
+        { eng: 'kakad arati aarati arti jay deva shri sadguru dnyan', hin: 'काकड आरती-जय देवा श्री सद्गुरु जय ज्ञान रुपा',  dir: 'ntn', id: '8' , bk: 'नाना तिरीचा नामरंग', pg: '8' },
+        { eng: 'kakad arati shri dattatreya prarthan chashk', hin: 'काकड आरती-श्री दत्तात्रेय प्रार्थना चतुष्क', dir: 'ntn', id: '9', bk: 'नाना तिरीचा नामरंग', pg: '9' },
+        { eng: 'kakad arati manas atm puja pooja', hin: 'काकड आरती-श्री मानस पूजा (श्री आत्मपूजा)', dir: 'ntn', id: '10', bk: 'नाना तिरीचा नामरंग', pg: '10' }, 
+        { eng: 'prabhati jago mohan pyare', hin: 'प्रभाती-जागो मोहन प्यारे', dir: 'ntn', id: '13', bk: 'नाना तिरीचा नामरंग', pg: '13' }, 
+     
+     
+    ];
 
 if (typeof module === 'object' && module.exports) {
     module.exports = ntn;
