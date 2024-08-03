feb0fa572866334a3558966ea90542235763ff23
shrinanabhajan <shrinanabhajan@gmail.com>
2024-08-03 13:02:09 +0530

    Update ntn.js

--- scripts/ntn.js
+++ scripts/ntn.js
@@ -5,6 +5,7 @@
         { eng: 'kakad arati shri dattatreya prarthan chashk', hin: 'काकड आरती-श्री दत्तात्रेय प्रार्थना चतुष्क', dir: 'ntn', id: '9', bk: 'नाना तिरीचा नामरंग', pg: '9' },
         { eng: 'kakad arati manas atm puja pooja', hin: 'काकड आरती-श्री मानस पूजा (श्री आत्मपूजा)', dir: 'ntn', id: '10', bk: 'नाना तिरीचा नामरंग', pg: '10' }, 
         { eng: 'prabhati jago mohan pyare', hin: 'प्रभाती-जागो मोहन प्यारे', dir: 'ntn', id: '13', bk: 'नाना तिरीचा नामरंग', pg: '13' }, 
+        { eng: 'prabhati jagiye raghunath kunvar kuvar', hin: 'प्रभाती-जागिये रघुनाथ कुँवर', dir: 'ntn', id: '13.2', bk: 'नाना तिरीचा नामरंग', pg: '13' }, 
         { eng: 'prabhati uth pandharichya raya', hin: 'प्रभाती-उठ पंढरीच्या राया', dir: 'ntn', id: '14', bk: 'नाना तिरीचा नामरंग', pg: '14' }, 
         { eng: 'prabhati uthi ba shri guruvara', hin: 'प्रभाती-उठी उठी बा श्री गुरुवरा मार्तंडा', dir: 'ntn', id: '14.2', bk: 'नाना तिरीचा नामरंग', pg: '14' }, 
      
