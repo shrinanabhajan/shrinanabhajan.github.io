72cefb367d7a7faec834b6c7bb773c07fc91e297
shrinanabhajan <shrinanabhajan@gmail.com>
2024-08-03 12:38:22 +0530

    Update ntn.js

--- scripts/ntn.js
+++ scripts/ntn.js
@@ -3,6 +3,7 @@
     { eng: 'kakad arati shri datta prabodh stav', hin: 'काकड आरती-श्री दत्त प्रभोधस्तव' , dir: 'ntn', id: '6.2' , bk: 'नाना तिरीचा नामरंग', pg: '6' },
     { eng: 'kakad arati aarati arti jay deva shri sadguru dnyan', hin: 'काकड आरती-जय देवा श्री सद्गुरु जय ज्ञान रुपा',  dir: 'ntn', id: '8' , bk: 'नाना तिरीचा नामरंग', pg: '8' },
     { eng: 'kakad arati shri dattatreya prarthan chashk', hin: 'काकड आरती-श्री दत्तात्रेय प्रार्थना चतुष्क', dir: 'ntn', id: '9', bk: 'नाना तिरीचा नामरंग', pg: '9' },
+    { eng: 'kakad arati manas atm puja pooja', hin: 'काकड आरती-श्री मानस पूजा (श्री आत्मपूजा)', dir: 'ntn', id: '10', bk: 'नाना तिरीचा नामरंग', pg: '10' },     
     
 ];
 
