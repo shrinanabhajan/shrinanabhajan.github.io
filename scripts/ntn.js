f8419242c0b26c8db0dccbda851e026784206db1
shrinanabhajan <shrinanabhajan@gmail.com>
2024-08-03 13:44:34 +0530

    Update ntn.js

--- scripts/ntn.js
+++ scripts/ntn.js
@@ -1,5 +1,5 @@
 ﻿var ntn = [
-        { eng: 'kakad arati bupali', hin: 'काकड आरती-भूपाळी', dir: 'ntn', id: '6.1', bk: 'नाना तिरीचा नामरंग', pg: '6' },
+        { eng: 'kakad arati bhupali', hin: 'काकड आरती-भूपाळी', dir: 'ntn', id: '6.1', bk: 'नाना तिरीचा नामरंग', pg: '6' },
         { eng: 'kakad arati shri datta prabodh stav', hin: 'काकड आरती-श्री दत्त प्रभोधस्तव' , dir: 'ntn', id: '6.2' , bk: 'नाना तिरीचा नामरंग', pg: '6' },
         { eng: 'kakad arati aarati arti jay deva shri sadguru dnyan', hin: 'काकड आरती-जय देवा श्री सद्गुरु जय ज्ञान रुपा',  dir: 'ntn', id: '8' , bk: 'नाना तिरीचा नामरंग', pg: '8' },
         { eng: 'kakad arati shri dattatreya prarthan chashk', hin: 'काकड आरती-श्री दत्तात्रेय प्रार्थना चतुष्क', dir: 'ntn', id: '9', bk: 'नाना तिरीचा नामरंग', pg: '9' },
