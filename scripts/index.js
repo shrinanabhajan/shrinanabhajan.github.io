a042660acd51883445b04dc2b3af5a557683e74d
shrinanabhajan <shrinanabhajan@gmail.com>
2024-08-05 02:04:44 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@
     "use strict";
     var searchBox, listElement, searchBtn, progressBar;
     //var allBhajans = p1.concat(p2, p3, p4);
-    var allBhajans = ntn.concat(b1);
+    var allBhajans = dt.concat(b1, ntn);
     var filteredBhajans = allBhajans;
     //var filteredBhajans = [];
     var timeout;
