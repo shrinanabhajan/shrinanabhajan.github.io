e8a46f35a46552a1d09992cd4dd26cbc20f40358
shrinanabhajan <shrinanabhajan@gmail.com>
2024-08-05 02:03:18 +0530

    Create dt.js

---
+++ scripts/dt.js
@@ -0,0 +1,11 @@
+var dt = [
+    { eng: 'devtai karu dhyan deva tumache', hin: 'करु ध्यान देवा तुमचे', dir: 'dt', id: '1', bk: '', pg: '',  },
+    
+
+
+
+];
+
+if (typeof module === 'object' && module.exports) {
+    module.exports = dt;
+}
